e46308eb022c6fecdaca5d886df901401c992baa
Gabriel Ami <gamihalachioaie@gmail.com>
2021-12-01 08:11:40 +0100

    add: second part solution

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -3,14 +3,11 @@ package day01
 import readInputForDay
 
 fun run() {
-    fun countLargerThanPrevious(input: List<String>): Int {
+    fun countLargerThanPrevious(input: List<Int>): Int {
         var previousMeasurement: Int = Int.MAX_VALUE
-        var measurement: Int = 0
         var count: Int = 0
 
-        for (rawMeasurement in input) {
-            measurement = rawMeasurement.toInt()
-
+        for (measurement in input) {
             if (measurement > previousMeasurement) {
                 count++
             }
@@ -21,14 +18,37 @@ fun run() {
         return count
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    // First part.
+    fun countLargerThanPrevious(input: List<String>): Int {
+        val intInput = ArrayList<Int>()
+
+        for (rawInput in input) {
+            intInput.add(rawInput.toInt())
+        }
+
+        return countLargerThanPrevious(intInput)
+    }
+
+    // Second part.
+    fun countLargerCompoundOfMeasurements(input: List<String>, compoundBy: Int): Int {
+        val compoundsCount = input.size - compoundBy + 1
+
+        val compounds = ArrayList<Int>()
+
+        for (compound in 0 until compoundsCount) {
+            compounds.add(input[compound].toInt() +
+                    input[compound + 1].toInt() +
+                    input[compound + 2].toInt())
+        }
+
+        return countLargerThanPrevious(compounds)
     }
 
     val testInput = readInputForDay(1, true)
     check(countLargerThanPrevious(testInput) == 7)
+    check(countLargerCompoundOfMeasurements(testInput, 3) == 5)
 
     val input = readInputForDay(1)
     println(countLargerThanPrevious(input))
-    println(part2(input))
+    println(countLargerCompoundOfMeasurements(input, 3))
 }
