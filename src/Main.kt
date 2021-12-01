049d01b17f0930c04ca699bc3b623fa6c8085ca5
Gabriel Ami <gamihalachioaie@gmail.com>
2021-12-01 06:53:47 +0100

    add: base for solving AoC puzzles

---
+++ src/Main.kt
@@ -0,0 +1,6 @@
+fun main()
+{
+    print("Select which day you desire to execute: ")
+
+    val day: Int? = readLine()?.toIntOrNull()
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -3,9 +3,18 @@ import java.math.BigInteger
 import java.security.MessageDigest
 
 /**
+ * Reads lines from the given input for a specific day, indicating whether to retrieve the test input.
+ */
+fun readInputForDay(day: Int, test: Boolean) : List<String> {
+    val name: String = String.format("Day%02d", day)
+
+    return readInput(name)
+}
+
+/**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(name: String) = File("src/${name.lowercase()}", "$name.txt").readLines()
 
 /**
  * Converts string to md5 hash.
