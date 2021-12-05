ff5b7fe9964532c4ca58417ec3366951ac4dd4f3
Gabriel Ami <gamihalachioaie@gmail.com>
2021-12-05 22:42:26 +0100

    add: day 03 second part solution

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -4,10 +4,14 @@ import readInputForDay
 
 fun run() {
     val testInput = readInputForDay(3, true)
-    val diagnosticDecoder: DiagnosticDecoder = DiagnosticDecoder(testInput)
+    val diagnosticDecoder: DiagnosticDecoder = DiagnosticDecoder(testInput, 5)
     check(diagnosticDecoder.powerConsumption() == 198)
+    check(diagnosticDecoder.oxygenGeneratorRating() == 23)
+    check(diagnosticDecoder.CO2ScrubberRating() == 10)
+    check(diagnosticDecoder.lifeSupportRating() == 230)
 
     val input = readInputForDay(3)
     diagnosticDecoder.updateInput(input, 12)
     println(diagnosticDecoder.powerConsumption())
+    println(diagnosticDecoder.lifeSupportRating())
 }
--- src/day03/DiagnosticDecoder.kt
+++ src/day03/DiagnosticDecoder.kt
@@ -2,33 +2,51 @@ package day03
 
 import kotlin.math.min
 
-class DiagnosticDecoder(private var diagnosticInput: List<String>, private var diagnosticInputBits: Int = 5) {
+class DiagnosticDecoder(private var diagnosticInput: List<String>, private var diagnosticInputBits: Int) {
     fun updateInput(diagnosticInput: List<String>, diagnosticInputBits: Int = 5) {
         this.diagnosticInput = diagnosticInput
         this.diagnosticInputBits = diagnosticInputBits
     }
 
+    fun powerConsumption() : Int {
+        val mostCommon: Int = mostCommonBitwiseValue()
+        val leastCommon: Int = leastCommonBitwiseValue(mostCommon)
+
+        return mostCommon * leastCommon
+    }
+
     private fun mostCommonBitwiseValue() : Int {
         var mostCommonBits: Int = 0
-
-        val bitsCount: IntArray = IntArray(diagnosticInputBits)
-        for (value in diagnosticInput) {
-            for (bit in 0 until diagnosticInputBits) {
-                if (value[bit].digitToInt() == 1) {
-                    bitsCount[bit] += 1
-                }
-            }
-        }
 
         for (bit in 0 until diagnosticInputBits) {
             mostCommonBits = mostCommonBits shl min(bit, 1)
 
-            if (bitsCount[bit] >= (diagnosticInput.size + 1) / 2) {
-                mostCommonBits += 0b1
-            }
+            // If the most common bit is 0, it just gets ignored.
+            mostCommonBits += mostCommonBit(diagnosticInput, bit)
         }
 
         return mostCommonBits
+    }
+
+    private fun mostCommonBit(list: List<String>, bit: Int, zeroIfEqual: Boolean = false) : Int {
+        var count: Int = 0
+        for (value in list) {
+            if (value[bit].digitToInt() == 1) {
+                count++
+            }
+        }
+
+        if (count == ((list.size + 1) / 2)) {
+            return if (zeroIfEqual)
+                0b0
+            else
+                0b1
+        }
+
+        return if (count > (list.size / 2))
+            0b1
+        else
+            0b0
     }
 
     private fun leastCommonBitwiseValue(commonBits: Int) : Int {
@@ -43,10 +61,55 @@ class DiagnosticDecoder(private var diagnosticInput: List<String>, private var d
         return commonBits.inv() and usableBits
     }
 
-    fun powerConsumption() : Int {
-        val mostCommon: Int = mostCommonBitwiseValue()
-        val leastCommon: Int = leastCommonBitwiseValue(mostCommon)
+    fun oxygenGeneratorRating() : Int {
+        return diagnosticCodeByBitExclusion(excludeLeastCommon = true, zeroGreaterPriorityThanOne = false)
+    }
 
-        return mostCommon * leastCommon
+    private fun diagnosticCodeByBitExclusion(excludeLeastCommon: Boolean, zeroGreaterPriorityThanOne: Boolean) : Int {
+        var diagnosticInputClone: ArrayList<String> = ArrayList(diagnosticInput)
+        var excludedIndexList: List<Int>
+
+        for (bit in 0 until diagnosticInputBits) {
+            excludedIndexList = excludeByBit(diagnosticInputClone, bit, excludeLeastCommon, zeroGreaterPriorityThanOne)
+
+            for (index in excludedIndexList.reversed()) {
+                diagnosticInputClone.removeAt(index)
+            }
+
+            if (diagnosticInputClone.size == 1) {
+                break
+            }
+        }
+
+        return diagnosticInputClone[0].toInt(2)
+    }
+
+    private fun excludeByBit(list: List<String>, bit: Int, excludeLeastCommon: Boolean,
+                             zeroGreaterPriorityThanOne: Boolean) : List<Int> {
+        var excludedIndexList: ArrayList<Int> = ArrayList()
+        var excludeByBit: Int = mostCommonBit(list, bit, zeroGreaterPriorityThanOne)
+
+        // Invert the bit if the least common bit is excluded.
+        if (excludeLeastCommon) {
+            excludeByBit = excludeByBit.inv() and 0b1
+        }
+
+        for (i in list.indices) {
+            val item: String = list[i]
+
+            if (item[bit].digitToInt() == excludeByBit) {
+                excludedIndexList.add(i)
+            }
+        }
+
+        return excludedIndexList
+    }
+
+    fun CO2ScrubberRating() : Int {
+        return diagnosticCodeByBitExclusion(excludeLeastCommon = false, zeroGreaterPriorityThanOne = false)
+    }
+
+    fun lifeSupportRating() : Int {
+        return oxygenGeneratorRating() * CO2ScrubberRating()
     }
 }
